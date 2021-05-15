3f605d935a67e288acb9fb335e10b32c19766cd8
unknown <ufocalypse@gmail.com>
2021-05-15 18:35:33 +0545

    Make Assets System more robust

--- src/index.js
+++ src/index.js
@@ -1,33 +1,58 @@
-import { Camera, Canvas2DRenderer } from "@lib"
+import { Camera, Canvas2DRenderer, Texture } from "@lib"
 import { startGameLoop } from "@utils"
 import config from "@config"
 import Wall from "@entities/Wall"
 import Player from "./entities/Player"
-import bgMusic from "./assets/audio/bg.wav"
+import bgMusicUrl from "./assets/audio/music2.wav"
+import AssetsCache from "@lib/utils/AssetsCache"
+import Sound from "@lib/utils/Sound"
 
-console.log({ bgMusic })
+console.log({ bgMusicUrl })
 
-const { viewport: canvasDimensions } = config
+const assetsCache = new AssetsCache()
+assetsCache.load([
+    { url: bgMusicUrl, msg: "Loading sound" }
+ ])
+Texture.initialize(assetsCache)
 
-const gameWorld = new Camera({ id: "root", viewport: canvasDimensions, world: { ...canvasDimensions, width: 1000 } })
+assetsCache.once("load", () => {
+    alert()
+    const { viewport: canvasDimensions } = config
+    const music = new Sound(assetsCache.get(bgMusicUrl))
+    music.play()
+    alert()
+    // music.volume = 50
+    // music.loop = true
 
-const wall = new Wall({ id: "wall" })
-const player = new Player({ width: 24, height: 24, fill: "goldenrod", id: "player" })
+    const gameWorld = new Camera({ id: "root", viewport: canvasDimensions, world: { ...canvasDimensions, width: 1000 } })
 
-gameWorld.setSubject(player)
-gameWorld.add(wall)
-gameWorld.add(player)
+    const wall = new Wall({ id: "wall" })
+    const player = new Player({ width: 24, height: 24, fill: "goldenrod", id: "player" })
 
-const renderer = new Canvas2DRenderer({ canvasId: "arena", scene: gameWorld, background: "black", ...canvasDimensions })
+    gameWorld.setSubject(player)
+    gameWorld.add(wall)
+    gameWorld.add(player)
 
-const mainUpdateFn = (dt) => { 
+    const renderer = new Canvas2DRenderer({ canvasId: "arena", scene: gameWorld, background: "black", ...canvasDimensions })
 
-}
+    const mainUpdateFn = dt => { 
+        // console.log(`Frame Rate: ${1 / dt}`)
+    }
 
-const loopControls = startGameLoop({
-    mainUpdateFn,
-    renderer
+    const loopControls = startGameLoop({
+        mainUpdateFn,
+        renderer
+    })
+    // loopControls.setSpeed(0.5)
+    // console.log(loopControls.meta.elapsed)
+    // setTimeout(() => loopControls.pause(), 6000)
 })
 
-// loopControls.setSpeed(0.5)
-// setTimeout(() => loopControls.pause(), 6000)
+assetsCache.on("error", e => {
+    console.log(e)
+})
+
+assetsCache.on("progress", ({ progress, msg }) => {
+    console.log(`Loading progress: ${progress}%`)
+    console.log(`Loading message: ${msg}`)
+})
