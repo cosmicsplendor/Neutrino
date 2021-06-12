5d2add93833b02169d077ec86e9e0be3d51e5c7f
unknown <ufocalypse@gmail.com>
2021-06-12 09:28:44 +0545

    Implement custom Viewport class

--- src/config/index.js
+++ src/config/index.js
@@ -1,20 +1,13 @@
+import Viewport  from "@utils/ViewPort"
 import { clamp } from "@utils/math"
-import Observable from "@utils/Observable"
 
-const computeViewport = () => {
-    return ({ 
-        width: clamp(300, 600, window.innerWidth),
-        height: window.innerHeight
-    })
-}
-const viewport = new Observable([ "change" ], computeViewport())
-window.addEventListener("resize", function(e) {
-    Object.assign(viewport, computeViewport())
-    viewport.emit("change", viewport)
+const computeViewport = () => ({ 
+    width: clamp(300, 600, window.innerWidth),
+    height: window.innerHeight
 })
 
 export default Object.freeze({
-    viewport,
+    viewport: new Viewport(computeViewport),
     worldWidth: 1000,
     worldHeight: window.innerHeight,
     gravity: 1000
