d7895a3e2b11473c0e0187c4c3123d32f3fa08bf
unknown <ufocalypse@gmail.com>
2021-06-18 18:28:06 +0545

    Add false camera clipping bug brought about by the breakage of parent linkage in levels

--- src/screens/Level/index.js
+++ src/screens/Level/index.js
@@ -9,7 +9,7 @@ import soundSpriteId from "@assets/audio/sprite.mp3"
 import soundMetaId from "@assets/audio/sprite.cson"
 
 class LevelScreen extends Camera {
-    background = "white"
+    background = "black"
     initialized = false
     constructor({ game }) {
         super({ id: "root", viewport: config.viewport })
@@ -25,23 +25,26 @@ class LevelScreen extends Camera {
             this.music = soundSprite.create("music", { volume: 1, pan: -1 })
             this.explosionSound = soundSprite.createPool("explosion") 
             // this.music.play()
-            this.player = new Player({ width: 80, height: 80, fill: "brown", id: "player", speed: 100, pos: { x: 300 } })
+            this.player = new Player({ width: 80, height: 80, fill: "brown", id: "player", speed: 500, pos: { x: 300 } })
             this.player.explosionSFX = this.explosionSound
             this.setSubject(this.player)
         })
     }
     setLevel(level) {
+        this.unsetLevel()
+        this.add(level)
+    }
+    unsetLevel() {
         if (this.children && this.children[0]) {
             Node.cleanupRecursively(this.children[0])
+            this.children.length = 0
         }
-        this.children = [ level ]
     }
     onEnter() { 
-        this.setLevel(new Level1({ player: this.player, assetsCache: this.game.assetsCache }))
+        this.setLevel(new Level1({ player: this.player, assetsCache: this.game.assetsCache, camera: this }))
     }
     onExit() {
-        Node.cleanupRecursively(this)
-        this.children.length = 0
+        this.unsetLevel()
     }
 }
 
--- src/screens/Level/levels/level1.js
+++ src/screens/Level/levels/level1.js
@@ -1,6 +1,7 @@
 import { Node } from "@lib"
 import { createAtlas } from "@lib/entities/TexRegion"
 import TiledLevel from "@utils/TiledLevel"
+import ParticleEmitter from "@utils/ParticleEmitter"
 
 import Crate from "@entities/Crate"
 import levelDataId from "@assets/levels/level.cson"
@@ -8,7 +9,7 @@ import texatlasId from "@assets/images/texatlas.png"
 import texatlasMetaId from "@assets/images/atlasmeta.cson"
 
 class Level1 extends Node {
-    constructor({ player, assetsCache}) {
+    constructor({ player, assetsCache, camera }) {
         super()
         const crate = new Crate({ id: "crate", width: 50, height: 50, pos: { x: 200, y: 0 } })
 
@@ -22,12 +23,15 @@ class Level1 extends Node {
         })
 
         arena.pos.y = 100
-        this.world =  { width: arena.width, height: window.innerHeight }
+        console.log(arena.width)
+        console.log(camera.bounds)
+        camera.world =  { width: 6000, height: window.innerHeight }
         this.player = player
 
         this.add(arena)
         this.add(crate)
         this.add(player)
+        this.add(new ParticleEmitter({ }))
     }
 
 }
