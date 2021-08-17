f48391fc16fc5495dc828f365378afa1ab75b29e
unknown <ufocalypse@gmail.com>
2021-08-17 10:48:45 +0545

    Enhance viewport computation logic

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,25 @@
 import Viewport  from "@utils/ViewPort"
-import { clamp } from "@utils/math"
 
-const computeViewport = () => ({ // spoorting resolution upto 1280 * 1280
-    width: clamp(300, 1280, window.innerWidth),
-    height: Math.min(1280, window.innerHeight)
-})
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+const resolution = isMobile ? { max: 1280, min: 720 }: { max: 1920, min: 1080 }
+const aspect = resolution.max / resolution.min
+const computeViewport = () => {
+    const width = window.innerWidth, height = window.innerHeight
+    const portraitMode = height > width
+    const maxWidth = portraitMode ? resolution.min: resolution.max
+    const maxHeight = portraitMode ? resolution.max: resolution.min
+    const vpWidth = Math.min(width, maxWidth)
+    const vpHeight = Math.min(height, maxHeight)
+    return ({
+        width: vpWidth,
+        height: vpHeight,
+    })
+}
 
 export default Object.freeze({
     viewport: new Viewport(computeViewport),
     worldWidth: 1000,
     worldHeight: window.innerHeight,
     gravity: 1700,
-    isMobile: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    isMobile
 })
