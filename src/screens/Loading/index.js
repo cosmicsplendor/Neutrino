c1aecf136938f7f24d782da071904dbdb3ba3560
unknown <ufocalypse@gmail.com>
2021-09-09 16:20:14 +0545

    Remove all usage of Rect to make integration of webgl renderer smoother

--- src/screens/Loading/index.js
+++ src/screens/Loading/index.js
@@ -1,8 +1,4 @@
 import { Node } from "@lib"
-import { Rect } from "@lib/entities"
-import { calcCentered } from "@utils/entity"
-
-import config from "@config"
 import { MAIN_MENU } from "@screens/names"
 
 import soundSprite from "@assets/audio/sprite.mp3"
@@ -22,15 +18,15 @@ class TitleScreen extends Node {
     constructor({ game }) { 
         super()
         this.game = game
-        const progressOutline = new Rect({ width: 250, height: 40, fill: "rgba(0, 0, 0, 0)", stroke: "whitesmoke", strokeWidth: 2 })
-        const progressBar = new Rect({ width: 0, height: 40, fill: "whitesmoke" })
-        this.progressBar = progressBar
-        this.realign = viewport => { 
-            progressBar.pos = progressOutline.pos = calcCentered(viewport, { width: 250, height: 40 })
-        }
-        this.add(progressBar)
-        this.add(progressOutline)
-        this.realign(config.viewport)
+        // const progressOutline = new Rect({ width: 250, height: 40, fill: "rgba(0, 0, 0, 0)", stroke: "whitesmoke", strokeWidth: 2 })
+        // const progressBar = new Rect({ width: 0, height: 40, fill: "whitesmoke" })
+        // this.progressBar = progressBar
+        // this.realign = viewport => { 
+        //     progressBar.pos = progressOutline.pos = calcCentered(viewport, { width: 250, height: 40 })
+        // }
+        // this.add(progressBar)
+        // this.add(progressOutline)
+        // this.realign(config.viewport)
     }
     onEnter() {
         const { assetsCache } = this.game
@@ -51,18 +47,18 @@ class TitleScreen extends Node {
 
         assetsCache.on("error", console.log)
             assetsCache.on("progress", progress => {
-            this.progressBar.width = progress * 250
+            // this.progressBar.width = progress * 250
         })
         assetsCache.on("progress", payload => {
-            this.progressBar.width = 250 * payload.progress
+            // this.progressBar.width = 250 * payload.progress
         })
         assetsCache.on("load", () => {
             this.game.switchScreen(MAIN_MENU)
         })
-        config.viewport.on("change", this.realign)
+        // config.viewport.on("change", this.realign)
     }
     onExit() {
-        config.viewport.off("change", this.realign)
+        // config.viewport.off("change", this.realign)
         this.game.disposeScreen(this)
     }
 }
