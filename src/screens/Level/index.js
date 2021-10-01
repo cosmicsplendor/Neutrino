c1cff9f96f29a86b974e694ec64a140358e50792
unknown <ufocalypse@gmail.com>
2021-10-01 14:25:25 +0545

    Implement level screen

--- src/screens/Level/index.js
+++ src/screens/Level/index.js
@@ -1,7 +1,27 @@
 import { Node } from "@lib"
+import levels from "@config/levels"
+import { GAME } from "@screens/names"
 
 class LevelScreen extends Node {
-
+    constructor({ game, uiRoot, storage }) {
+        super()
+        this.game = game
+        this.storage = storage
+        this.uiRoot = uiRoot
+    }
+    onEnter() {
+        this.teardownUI = this.initUI({
+            onStart: level => {
+                this.game.switchScreen(GAME, level)
+            },
+            curLevel: this.storage.getCurLevel(),
+            uiRoot: this.uiRoot,
+            levels
+        })
+    }
+    onExit() {
+        this.teardownUI()
+    }
 }
 
 export default LevelScreen
