4e8ad0fb16af010e11183fa1ba6ad54a8462fe9c
unknown <ufocalypse@gmail.com>
2021-10-02 07:05:20 +0545

    Ingetrate imgBtn with existing modules

--- src/screens/MainMenu/initUI.js
+++ src/screens/MainMenu/initUI.js
@@ -1,23 +1,14 @@
 import UI from "@utils/UI"
 import config from "@config"
 import { calcCentered } from "@utils/entity"
-import styles from "./style.css"
+import imgBtn from "@screens/ui/imgBtn"
 const BTN_ID = "play-btn"
-const render = (btnImg) => 
-`
-<div 
-    class="${styles.playBtn}" 
-    id="${BTN_ID}" 
-    style="width: ${btnImg.width}px; height: ${btnImg.height}px; background: url(${btnImg.src});"
->
-</div>
-`
 
 const doc = new UI(document)
 
 const initUI = ({ uiRoot, btnImg, onPlay }) => {
     console.log(btnImg)
-    uiRoot.content = render(btnImg)
+    uiRoot.content = imgBtn(BTN_ID, btnImg)
     const playBtn = uiRoot.get(`#${BTN_ID}`)
     const playBtnBounds = playBtn.bounds
     const realign = viewport => { 
--- src/screens/ui/ImgBtn.js
+++ src/screens/ui/ImgBtn.js
@@ -2,7 +2,7 @@ import styles from "./style.css"
 export default (id, img) => {
     return `
     <div 
-        class="${styles.playBtn}" 
+        class="${styles.imgBtn}" 
         id="${id}" 
         style="width: ${img.width}px; height: ${img.height}px; background: url(${img.src});"
     >
