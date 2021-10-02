0d87026ed1382e70033f5a4cc9d36855f1375c7d
unknown <ufocalypse@gmail.com>
2021-10-02 07:32:29 +0545

    Add an option to pass in custom classname to imgBtn

--- src/screens/ui/ImgBtn.js
+++ src/screens/ui/ImgBtn.js
@@ -1,8 +1,8 @@
 import styles from "./style.css"
-export default (id, img) => {
+export default (id, img, class_) => {
     return `
     <div 
-        class="${styles.imgBtn}" 
+        class="${styles.imgBtn} ${class_ || ''}" 
         id="${id}" 
         style="width: ${img.width}px; height: ${img.height}px; background: url(${img.src});"
     >
