77102759ea3b2d5fc257ce5f77e4852b4b5cb7a8
unknown <ufocalypse@gmail.com>
2021-10-02 08:38:39 +0545

    Get it over with level selection ui items alignment

--- src/screens/Level/initUI.js
+++ src/screens/Level/initUI.js
@@ -7,10 +7,17 @@ import styles from "./style.css"
 const PREV = "prev-btn"
 const NEXT = "next-btn"
 const START = "start-btn"
+const INFO = "lev-info"
 
+const levelInfo = (level, levels) => {
+    return `
+        <div class="${styles.infoTitle}" id="${INFO}">${"Level " + level}</div>
+    `
+}
 const render = (images, level, levels) => {
     return `
         ${imgBtn(PREV, images.arrow, styles.prevBtn)}
+        ${levelInfo(level, levels)}
         ${imgBtn(NEXT, images.arrow)}
         ${imgBtn(START, images.resume)}
     `
@@ -23,9 +30,13 @@ export default ({ onStart, uiRoot, curLevel, images }) => {
     const prevBtn = uiRoot.get(`#${PREV}`)
     const nextBtn = uiRoot.get(`#${NEXT}`)
     const startBtn = uiRoot.get(`#${START}`)
+    const levelInfo = uiRoot.get(`#${INFO}`)
 
     const realign = viewport => {
+        console.log(prevBtn.bounds)
+        console.log(levelInfo.bounds)
         prevBtn.pos = calcAligned(viewport, prevBtn.bounds, "left", "center", 50)
+        levelInfo.pos = calcAligned(viewport, levelInfo.bounds, "center", "center", 0, 0)
         nextBtn.pos = calcAligned(viewport, nextBtn.bounds, "right", "center", -50)
         startBtn.pos = calcAligned(viewport, startBtn.bounds, "center", "bottom", 0,  -100)
     }
