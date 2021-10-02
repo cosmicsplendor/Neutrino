4552ffa8d2f8f021fa777384aaa2d5fdb7e0e3df
unknown <ufocalypse@gmail.com>
2021-10-02 07:52:50 +0545

    Finish level selection screen ui element realignment

--- src/screens/Level/initUI.js
+++ src/screens/Level/initUI.js
@@ -1,6 +1,8 @@
+import { calcAligned } from "@lib/utils/entity"
+import config from "@config"
 import levels from "@config/levels"
 import imgBtn from "@screens/ui/imgBtn"
-import "./style.css"
+import styles from "./style.css"
 
 const PREV = "prev-btn"
 const NEXT = "next-btn"
@@ -8,16 +10,29 @@ const START = "start-btn"
 
 const render = (images, level, levels) => {
     return `
-        ${imgBtn(PREV, images.arrow)}
+        ${imgBtn(PREV, images.arrow, styles.prevBtn)}
         ${imgBtn(NEXT, images.arrow)}
         ${imgBtn(START, images.resume)}
     `
 }
 
-export default ({ onStart, uiRoot, curLevel, levels, images }) => {
-    let level = curLevel
+export default ({ onStart, uiRoot, curLevel, images }) => {
     uiRoot.content = render(images, curLevel, levels)
+    let levelState = curLevel
+
+    const prevBtn = uiRoot.get(`#${PREV}`)
+    const nextBtn = uiRoot.get(`#${NEXT}`)
+    const startBtn = uiRoot.get(`#${START}`)
+
+    const realign = viewport => {
+        prevBtn.pos = calcAligned(viewport, prevBtn.bounds, "left", "center", 50)
+        nextBtn.pos = calcAligned(viewport, nextBtn.bounds, "right", "center", -50)
+        startBtn.pos = calcAligned(viewport, startBtn.bounds, "center", "bottom", 0,  -100)
+    }
+    config.viewport.on("change", realign)
+    realign(config.viewport)
     return () => {
         uiRoot.clear()
+        config.viewport.off("change", realign)
     }
 }
