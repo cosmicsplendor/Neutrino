2f62f702244c1b62d6cf1b05fe2d4f2862e6f718
unknown <ufocalypse@gmail.com>
2021-10-02 16:28:05 +0545

    Compute btn alignment efficiently

--- src/screens/Game/levels/initUI.js
+++ src/screens/Game/levels/initUI.js
@@ -1,11 +1,16 @@
 import { calcAligned } from "@utils/entity"
 import config from "@config"
 import imgBtn from "@screens/ui/imgBtn"
+import styles from "./style.css"
+
 const margin = 20
+const orbExpAmt = 2
 
 const PAUSE = "pause-btn"
 const RESUME  = "resume-btn"
 const ORB_IND = "orb-ind"
+const ORB_EXP = "orb-exp" // orb expend
+const ORB_EXP_IND = "orb-exp-ind"
 const CROSS = "cross-btn"
 const RESET = "reset-btn"
 
@@ -14,6 +19,8 @@ const render = (images) => {
         ${imgBtn(ORB_IND, images.orb)}
         ${imgBtn(PAUSE, images.pause)}
         ${imgBtn(RESUME, images.resume)}
+        ${imgBtn(ORB_EXP_IND, images.orb)}
+        <div id="${ORB_EXP}" class="${styles.orbExp}"> &times; ${orbExpAmt} </div>
         ${imgBtn(RESET, images.reset)}
         ${imgBtn(CROSS, images.cross)}
     `
@@ -23,6 +30,8 @@ export default (uiRoot, player, images) => {
     uiRoot.content = render(images)
     const ctrlBtns = config.isMobile && player.getCtrlBtns()
     const orbInd = uiRoot.get(`#${ORB_IND}`)
+    const orbExp = uiRoot.get(`#${ORB_EXP}`)
+    const orbExpInd = uiRoot.get(`#${ORB_EXP_IND}`)
     const pauseBtn = uiRoot.get(`#${PAUSE}`)
     const resumeBtn = uiRoot.get(`#${RESUME}`)
     const restartBtn = uiRoot.get(`#${RESET}`)
@@ -33,12 +42,14 @@ export default (uiRoot, player, images) => {
               .add(ctrlBtns.axn)
     }
     const realign = viewport => {
-        const restartBB = restartBtn.bounds
-        orbInd.pos = calcAligned(viewport, orbInd.bounds, "left", "top", margin, margin)
-        pauseBtn.pos = calcAligned(viewport, pauseBtn.bounds, "right", "top", -margin, margin)
-        resumeBtn.pos = calcAligned(viewport, resumeBtn.bounds, "center", "center", 0, -restartBB.height - (margin / 2))
-        restartBtn.pos = calcAligned(viewport, restartBB, "center", "center")
-        crossBtn.pos = calcAligned(viewport, crossBtn.bounds, "center", "center", 0, restartBB.height + (margin / 2))
+        const orbExpBounds = orbExp.bounds
+        orbInd.pos = calcAligned(viewport, images.orb, "left", "top", margin, margin)
+        pauseBtn.pos = calcAligned(viewport, images.pause, "right", "top", -margin, margin)
+        resumeBtn.pos = calcAligned(viewport, images.resume, "center", "center", 0, -images.reset.height - (margin / 2))
+        orbExpInd.pos = calcAligned(viewport, images.orb, "center", "center", (images.reset.width / 2) + margin + images.orb.width / 2, -images.reset.height - (margin / 2))
+        orbExp.pos = calcAligned(viewport, orbExpBounds, "center", "center", (images.reset.width / 2) + margin * 3 / 2 +  images.orb.width + orbExpBounds.width / 2, -images.reset.height - (margin / 2))
+        restartBtn.pos = calcAligned(viewport, images.reset, "center", "center")
+        crossBtn.pos = calcAligned(viewport, images.cross, "center", "center", 0, images.reset.height + (margin / 2))
         if (!ctrlBtns) { return }
         ctrlBtns.left.pos = calcAligned(viewport, ctrlBtns.left.bounds, "left", "bottom", margin, -margin)
         ctrlBtns.right.pos = calcAligned(viewport, ctrlBtns.right.bounds, "left", "bottom", 40 + (ctrlBtns.left.bounds.width), -margin)
