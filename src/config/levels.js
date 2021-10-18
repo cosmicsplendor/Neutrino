b8a7adfe55f698e9d4831097cf7e2ea994b633b9
unknown <ufocalypse@gmail.com>
2021-10-18 15:36:39 +0545

    Add level 6

--- src/config/levels.js
+++ src/config/levels.js
@@ -3,30 +3,30 @@ import level2 from "@assets/levels/level2.cson"
 import level3 from "@assets/levels/level3.cson"
 import level4 from "@assets/levels/level4.cson"
 import level5 from "@assets/levels/level5.cson"
+import level6 from "@assets/levels/level6.cson"
 export default [
     {
         id: level1,
-        bg: "#2e2e3d",
         music: "music3"
     },
     {
         id: level2,
-        bg: "#121218",
         music: "music2"
     },
     {
         id: level3,
-        bg: "#2e2e3d",
         music: "music4"
     },
     {
         id: level4,
-        bg: "#121218",
         music: "music1"
     },
     {
         id: level5,
-        bg: "#10103a",
         music: "music3"
     },
+    {
+        id: level6,
+        music: "music1"
+    }
 ]
