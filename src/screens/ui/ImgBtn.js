e436ece726cb05cc78858301c4251badf19ef076
unknown <ufocalypse@gmail.com>
2021-10-24 12:17:09 +0545

    Add support for imgBtn tooltip

--- src/screens/ui/ImgBtn.js
+++ src/screens/ui/ImgBtn.js
@@ -1,20 +1,22 @@
 import UI from "@utils/UI"
 import styles from "./style.css"
 
-export const dom = (img, class_) => {
+export const dom = (img, class_, title="") => {
     const el = UI.create("div")
     el.domNode.setAttribute("class", `${styles.imgBtn} ${class_ || ''}`)
     el.domNode.setAttribute("style", `width: ${img.width}px; height: ${img.height}px; background: url(${img.src});`)
+    el.domNode.setAttribute("title", title)
     el.width = img.width
     el.height = img.height
     return el
 }
-export default (id, img, class_) => {
+export default (id, img, class_, title="") => {
     return `
     <div 
         class="${styles.imgBtn} ${class_ || ''}" 
         id="${id}" 
         style="width: ${img.width}px; height: ${img.height}px; background: url(${img.src});"
+        title="${title}"
     >
     </div>
     `
