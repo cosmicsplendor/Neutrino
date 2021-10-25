46fc8f711b26656f693510165d2cff1177d957cb
unknown <ufocalypse@gmail.com>
2021-10-25 12:47:27 +0545

    Mandate play button click for navigating to level selection screen, removing keypress triggered navigation, to make sure that the game gets user interaction for audio playback

--- src/screens/MainMenu/initUI.js
+++ src/screens/MainMenu/initUI.js
@@ -1,4 +1,3 @@
-import UI from "@utils/UI"
 import config from "@config"
 import { calcAligned } from "@utils/entity"
 import btn from "@screens/ui/btn"
@@ -6,20 +5,17 @@ import btn from "@screens/ui/btn"
 const BTN_ID = "play-btn"
 
 const initUI = ({ uiRoot, onPlay }) => {
-    const doc = new UI(document)
     uiRoot.content = btn(BTN_ID, "PLAY")
     const playBtn = uiRoot.get(`#${BTN_ID}`)
     const realign = viewport => { 
         playBtn.pos = calcAligned(viewport, playBtn, "center", "center", 0, 0)
     }
     playBtn.on("click", onPlay)
-    if (config.isMobile === false) doc.on("keydown", onPlay)
     config.viewport.on("change", realign)
     realign(config.viewport)
     return () => {
         playBtn.off("click", onPlay)
         uiRoot.clear()
-        if (config.isMobile === false) doc.off("keydown", onPlay)
         config.viewport.off("change", realign)
     }
 }
