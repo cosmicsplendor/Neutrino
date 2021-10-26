1533ab29218ce25d8f2a8b46c23730a800f61d95
unknown <ufocalypse@gmail.com>
2021-10-26 07:23:26 +0545

    Remove support for imgBtn title

--- src/screens/ui/ImgBtn.js
+++ src/screens/ui/ImgBtn.js
@@ -1,22 +1,20 @@
 import UI from "@utils/UI"
 import styles from "./style.css"
 
-export const dom = (img, class_, title="") => {
+export const dom = (img, class_) => {
     const el = UI.create("div")
     el.domNode.setAttribute("class", `${styles.imgBtn} ${class_ || ''}`)
     el.domNode.setAttribute("style", `width: ${img.width}px; height: ${img.height}px; background: url(${img.src});`)
-    el.domNode.setAttribute("title", title)
     el.width = img.width
     el.height = img.height
     return el
 }
-export default (id, img, class_, title="") => {
+export default (id, img, class_) => {
     return `
     <div 
         class="${styles.imgBtn} ${class_ || ''}" 
         id="${id}" 
         style="width: ${img.width}px; height: ${img.height}px; background: url(${img.src});"
-        title="${title}"
     >
     </div>
     `
