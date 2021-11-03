cf3945533ef1b8b5e4e1fff049a674ea94e9aeea
unknown <ufocalypse@gmail.com>
2021-11-03 16:03:54 +0545

    Add tutorial ball to level 3

--- src/config/index.js
+++ src/config/index.js
@@ -11,14 +11,14 @@ import GPixStorage from "../helpers/storage/strategies/GPixStorage"
  */
 
 const resolutions = {
-    standard: { max: 1000, min: 750 }
+    standard: { max: 1080, min: 750 }
 }
 
 const desktopRes = resolutions.standard
 
 const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
 const scale = false
-const maxDpr = 1.15
+const maxDpr = 1
 const maxMobileDpr = 1.5
 const computeViewport = () => {
     const width = window.innerWidth, height = window.innerHeight // drawing buffer dimensions (how manny actual pixels are there in the screen regardless of scaling)
@@ -56,7 +56,7 @@ export default Object.freeze({
     get devicePixelRatio() {
         return Math.min(isMobile ? maxMobileDpr: maxDpr, window.devicePixelRatio)
     },
-    SDKStrat: null,
-    StorageStrat: LocStorage,
+    SDKStrat: GPix,
+    StorageStrat: GPixStorage,
     orientation: "portrait"
 })
