af401079a14602ca0aaaf265f769810b479e87fd
Umesh KC <ufocalypse@gmail.com>
2024-09-30 15:09:56 +0545

    Return union polygon edges in the format that's integrable to the generator without friction

--- test1.js
+++ test1.js
@@ -15,6 +15,15 @@ const calcComposite = entities => { // compute a rect that contains all the enti
 
 class XPointer {
   edges=[]
+  constructor(normalDir) {
+    this.normal = normalDir
+  }
+  eneumerate() {
+    return this.edges.map(e => {
+      const { x1, y1, x2 } = e
+      return { x: x1, y: y1, w: x2 - x1, h: 0, normal: this.normal }
+    })
+  }
   record(x, y) {
     const lastEdge = this.edges[this.edges.length - 1]
     const extendExisting = y === lastEdge?.y2
@@ -29,6 +38,15 @@ class XPointer {
 }
 class YPointer {
   edges=[]
+  constructor(normalDir) {
+    this.normal = normalDir
+  }
+  eneumerate() {
+    return this.edges.map(e => {
+      const { x1, y1, y2 } = e
+      return { x: x1, y: y1, w: 0, h: y2 - y1, normal: this.normal }
+    })
+  }
   record(x, y) {
     const lastEdge = this.edges[this.edges.length - 1]
     const extendExisting = x === lastEdge?.x2
@@ -66,10 +84,10 @@ const generateGrid = (rects) => {
 
 const computeEdges = rects => {
   const grid = generateGrid(rects)
-  const topPointer = new XPointer()
-  const bottomPointer = new XPointer()
-  const leftPointer = new YPointer()
-  const rightPointer = new YPointer()
+  const topPointer = new XPointer("top")
+  const bottomPointer = new XPointer("bottom")
+  const leftPointer = new YPointer("left")
+  const rightPointer = new YPointer("right")
   for (let x = 0; x < grid.w; x++) {
     let ytop = 0, ybottom = grid.h - 1
     while (!grid.get(x, ytop)) {
@@ -93,7 +111,9 @@ const computeEdges = rects => {
     }
     rightPointer.record(xright + 1, y)
   }
-  return [rightPointer, leftPointer, topPointer, bottomPointer].flatMap(p => p.edges)
+  return [rightPointer, leftPointer, topPointer, bottomPointer].flatMap(p => {
+    return p.eneumerate()
+  })
 }
 
 const rectangles = [
@@ -101,3 +121,4 @@ const rectangles = [
   { x: 2, y: 0, w: 2, h: 2 },
   { x: 1, y: 2, w: 2, h: 2 }
 ]
+console.log(computeEdges(rectangles))
