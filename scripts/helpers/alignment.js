f1c807aadb97ec7421ebec7c5b70f3d810f9ad6d
Umesh KC <ufocalypse@gmail.com>
2024-10-02 08:15:16 +0545

    Code an alignment map for transforming colloquial alignment to the one accepted by calcAligned function

--- scripts/helpers/alignment.js
+++ scripts/helpers/alignment.js
@@ -15,8 +15,20 @@ const getDims = async key => {
     return atlas[key]
 }
 
+const alignmentMap = {
+    "top-left": [ "left", "top" ],
+    "bottom-left": [ "left", "bottom" ],
+    "top": ["center", "top"],
+    "bottom": ["center", "bottom"],
+    "top-right": ["right", "top"],
+    "bottom-right": ["right", "bottom"],
+    "center": ["center", "center"],
+    "left": ["left", "center"],
+    "right": ["right", "center"]
+}
+
 const align = async (name, projection, alignment, dx, dy) => {
-    const [ alignX, alignY ] = alignment.split("-")
+    const [ alignX, alignY ] = alignmentMap[alignment]
     const dims = name === "checkpoint" ? { width: 0, height: 0}: await getDims(name)
     const aligned = calcAligned(convertToWorld(projection), { w: dims.width, h: dims.height }, alignX, alignY, dx, dy)
     return aligned
