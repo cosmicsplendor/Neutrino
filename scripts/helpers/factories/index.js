3fa63f8a49bd73adb3fa663e699d27c83dbb4531
Umesh KC <ufocalypse@gmail.com>
2024-10-05 20:48:10 +0545

    Fix a bus factory bug

--- scripts/helpers/factories/index.js
+++ scripts/helpers/factories/index.js
@@ -90,12 +90,19 @@ const factories = Object.freeze({
     },
     vlhd: lasers(),
     hlhd: lasers(),
+    crane: {
+        create: params => {
+            const { alignment, x, y, name } = params
+            return { x, y: y + (alignment.includes("top") ? 32: 0), name }
+        }
+    },
     bus: {
         fields: ['toX', 'toY', 'period'], // Based on Bus constructor
         dims: () => ({ width: 88, height: 88 }),
         create: (params) => {
-            const { toX, toY, x, y, name } = params
-            return { x, y, name, toX: x + Number(toX) * TILE_SIZE, y: y + Number(toY) * TILE_SIZE, period: +period }
+            const { toX, toY, x, y, name, period, alignment } = params
+            console.log({ groupId: "col-rects", x, y: y + (alignment.startsWith("top") ? 32: 0), name, toX: x + Number(toX) * TILE_SIZE, y: y + Number(toY) * TILE_SIZE, period: +period })
+            return { groupId: "col-rects", x, y: y + (alignment.startsWith("top") ? 32: 0), name, toX: x + Number(toX) * TILE_SIZE, toY: y + Number(toY) * TILE_SIZE, period: +period }
         }
     },
     magnet: {
