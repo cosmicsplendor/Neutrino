85dea2b69d2f0c6b191eaf54dda956b69d0030f1
Umesh KC <ufocalypse@gmail.com>
2024-10-21 13:36:30 +0545

    Automate laser length estimation

--- scripts/helpers/factories/lasers.js
+++ scripts/helpers/factories/lasers.js
@@ -1,5 +1,4 @@
 const TILE_SIZE = 48
-
 const lasers = (orientation) => {
     const fields = ['speed', 'period', 'on']
     if (orientation === "vertical") {
@@ -17,12 +16,12 @@ const lasers = (orientation) => {
             return true
         },
         create: (params) => {
-            const { x, y, toX, toY, speed, num, period, on, name, projection } = params
+            const { x, y, toX, toY, speed, period, on, name, projection } = params
             return {
                 x, y,
                 toX: x + Number(toX) * TILE_SIZE, toY: y + Number(toY) * TILE_SIZE,
                 name: name, on: Boolean(on),
-                period: +period, speed: +speed, num: +num
+                period: +period, speed: +speed, num: orientation === "horizontal" ? projection.w: projection.h
             }
         }
     }
