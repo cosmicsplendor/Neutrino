f528dda1c2acc27565885121eba780b89ab031b0
Umesh KC <ufocalypse@gmail.com>
2024-10-22 12:12:34 +0545

    Make testMode value accissible to computeViewport function

--- src/config/create.js
+++ src/config/create.js
@@ -21,47 +21,55 @@ const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/
 const scale = false
 const maxDpr = 1
 const maxMobileDpr = 1.5
-const computeViewport = () => {
-    const width = window.innerWidth, height = window.innerHeight // drawing buffer dimensions (how manny actual pixels are there in the screen regardless of scaling) / devicePixelRatio
-    const portraitMode = height > width
-    if (isMobile) {
-        /**
-         * 100% of the smaller side
-         * and max(100% of the smaller side, 70% of the bigger side)
-         */
-        const vpWidth = portraitMode ? width: Math.max(width, height)
-        const vpHeight = portraitMode ? Math.max(height, width): height
-        return ({
-            width: vpWidth,
-            height: vpHeight
-        })
-    }
-    const maxWidth = portraitMode ? desktopRes.min: desktopRes.max
-    const maxHeight = portraitMode ? desktopRes.max: desktopRes.min
-    const vpWidth = Math.min(width, maxWidth)
-    const vpHeight = Math.min(height, maxHeight)
-    return ({ // canvas dimensions
-        width: vpWidth,
-        height: vpHeight,
-    })
-    return ({ // canvas dimensions
-        width: window.innerWidth,
-        height: window.innerHeight,
-    })
-}
-const defaultConfig = {
-    viewport: new Viewport(computeViewport),
-    storageId: "jshdf190",
-    worldWidth: 1000,
-    worldHeight: window.innerHeight,
-    gravity: 1700,
-    isMobile,
-    scale,
-    get devicePixelRatio() {
-        if (this.testMode) return 1
-        return Math.min(isMobile ? maxMobileDpr: maxDpr, window.devicePixelRatio)
-    },
-    orientation: "portrait",
-}
 
-export default overrides => Object.assign(defaultConfig, overrides)
+
+
+export default overrides => {
+    const testMode = overrides.testMode
+    const computeViewport = () => {
+        const width = window.innerWidth, height = window.innerHeight // drawing buffer dimensions (how manny actual pixels are there in the screen regardless of scaling) / devicePixelRatio
+        const portraitMode = height > width
+        if (isMobile) {
+            /**
+             * 100% of the smaller side
+             * and max(100% of the smaller side, 70% of the bigger side)
+             */
+            const vpWidth = portraitMode ? width: Math.max(width, height)
+            const vpHeight = portraitMode ? Math.max(height, width): height
+            return ({
+                width: vpWidth,
+                height: vpHeight
+            })
+        }
+        if (testMode) {
+            return {
+                width: window.innerWidth,
+                height: window.innerHeight
+            }
+        }
+        const maxWidth = portraitMode ? desktopRes.min: desktopRes.max
+        const maxHeight = portraitMode ? desktopRes.max: desktopRes.min
+        const vpWidth = Math.min(width, maxWidth)
+        const vpHeight = Math.min(height, maxHeight)
+        return ({ // canvas dimensions
+            width: vpWidth,
+            height: vpHeight,
+        })
+        
+    }
+    const defaultConfig = {
+        viewport: new Viewport(computeViewport),
+        storageId: "jshdf190",
+        worldWidth: 1000,
+        worldHeight: window.innerHeight,
+        gravity: 1700,
+        isMobile,
+        scale,
+        get devicePixelRatio() {
+            if (this.testMode) return 1
+            return Math.min(isMobile ? maxMobileDpr: maxDpr, window.devicePixelRatio)
+        },
+        orientation: "portrait",
+    }
+    return Object.assign(defaultConfig, overrides)
+}
