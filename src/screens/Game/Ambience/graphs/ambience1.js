87c311e345ab86af7a31dc00b43ba463881783fa
Umesh KC <ufocalypse@gmail.com>
2024-12-06 18:27:52 +0545

    Improve ambience audio

--- src/screens/Game/Ambience/graphs/ambience1.js
+++ src/screens/Game/Ambience/graphs/ambience1.js
@@ -2,29 +2,20 @@ import SoundGraph from "../SoundGraph";
 
 const graph = new SoundGraph()
 
-graph.addNode("mel2_1")
-graph.addNode("mel2_2", [1,2])
-graph.addNode("mel2_3")
+graph.addNode("mel4")
 graph.addNode("creepy")
 graph.addNode("wind_1")
 graph.addNode("wind_2")
 
-graph.addEdge("mel2_1", "mel2_2", 1,0)
-
-graph.addEdge("mel2_2", "mel2_3", 1,0)
-
-graph.addEdge("mel2_3", "wind_2", 1)
-graph.addEdge("mel2_3", "wind_1", 1.5)
-graph.addEdge("mel2_3", "creepy", 1)
+graph.addEdge("mel4", "wind_2", 1)
+graph.addEdge("mel4", "wind_1", 1)
+graph.addEdge("mel4", "creepy", 2)
 
 graph.addEdge("creepy", "wind_1", 1.5)
 graph.addEdge("creepy", "wind_2", 1)
 
-graph.addEdge("wind_1", "mel2_1")
-graph.addEdge("wind_2", "mel2_1")
-
-graph.excludeRandom("mel2_3")
-
+graph.addEdge("wind_1", "mel4")
+graph.addEdge("wind_2", "mel4")
 
 graph.commit()
 
--- src/screens/Game/Ambience/graphs/ambience2.js
+++ src/screens/Game/Ambience/graphs/ambience2.js
@@ -2,17 +2,16 @@ import SoundGraph from "../SoundGraph";
 
 const graph = new SoundGraph()
 
-graph.addNode("jingl_2", [1,2])
+graph.addNode("jingl_1", [1,2])
 graph.addNode("flute_amb_1")
 graph.addNode("flute_amb_2")
 graph.addNode("wind_1")
 graph.addNode("wind_2")
 graph.addNode("wind_3")
-graph.addNode("jingl_1")
 graph.addNode("creepy")
 
-graph.addEdge("jingl_2", "flute_amb_1")
-graph.addEdge("jingl_2", "flute_amb_2")
+graph.addEdge("jingl_1", "flute_amb_1")
+graph.addEdge("jingl_1", "flute_amb_2")
 
 graph.addEdge("flute_amb_1", "wind_3")
 graph.addEdge("flute_amb_2", "wind_2")
@@ -23,7 +22,7 @@ graph.addEdge("wind_3", "jingl_1")
 graph.addEdge("jingl_1", "wind_1")
 
 graph.addEdge("wind_1", "creepy")
-graph.addEdge("creepy", "jingl_2")
+graph.addEdge("creepy", "jingl_1")
 
 graph.commit()
 
